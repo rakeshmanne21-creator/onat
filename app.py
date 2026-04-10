3e58f7a169b5ce479ab91130d7fe673c397cfad1
rakeshmanne21-creator <stylishrakesh986@gmail.com>
2026-04-11 00:14:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -780,5 +780,5 @@ def close_db(error):
 
 if __name__ == "__main__":
     load_known_faces()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=False)
